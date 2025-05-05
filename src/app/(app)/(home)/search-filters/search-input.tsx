6759ec5b5c0c12a9cfe8a9e86b941bed653b1d87
Sandro <alexanderkapanadze52@gmail.com>
2025-05-05 16:22:49 +0200

    started implementing categories

---
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -0,0 +1,26 @@
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import React from "react"
+
+interface Props {
+  disabled?: boolean
+}
+
+const SearchInput: React.FC<Props> = ({ disabled = false }) => {
+  return (
+    <div className="flex items-center gap-2 w-full">
+      <div className="relative w-full">
+        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
+        <Input
+          className="pl-8"
+          placeholder="Search products"
+          disabled={disabled}
+        />
+      </div>
+      {/* TODO: Add categories view all Button */}
+      {/* TODO: Add library Button */}
+    </div>
+  )
+}
+
+export default SearchInput
