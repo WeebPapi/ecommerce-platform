428227d9b186b870cf4cb1dc3c2449a40f20acbd
Sandro <alexanderkapanadze52@gmail.com>
2025-05-11 21:15:26 +0200

    started implementing trpc

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,3 +1,15 @@
+"use client"
+
+import { useQuery } from "@tanstack/react-query"
+import { useTRPC } from "@/trpc/client"
+
 export default function Home() {
-  return <div className="flex flex-col gap-y-4 p-4"></div>
+  const trpc = useTRPC()
+  const categories = useQuery(trpc.categories.getMany.queryOptions())
+
+  return (
+    <div className="flex flex-col gap-y-4 p-4">
+      {JSON.stringify(categories.data, null, 2)}
+    </div>
+  )
 }
